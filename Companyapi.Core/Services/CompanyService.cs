229e4485ff9c31ecb97bcc05ab3b99ca254e9996
omar52323 <omar.jaramillo@movinova.com>
2025-02-03 20:56:33 -0500

    Agregar archivos de proyecto.

---
+++ Companyapi.Core/Services/CompanyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Companyapi.Domain.Interfaces.Services;
+
+namespace Companyapi.Core.Services
+{
+    public class CompanyService:ICompanyService
+    {
+
+    }
+}
